b761abcb4b714711b113a473577a03a84951783b
Leonardo34 <leo_broch@hotmail.com>
2017-03-27 15:44:34 -0300

    Add attributes and getters/setters in Chromosome class

--- src/br/com/unisinos/PokemonFinder/Chromosome.java
+++ src/br/com/unisinos/PokemonFinder/Chromosome.java
@@ -1,5 +1,27 @@
 package br.com.unisinos.PokemonFinder;
 
 public class Chromosome {
-
+	private int[] genes;
+	private int fitness;
+	
+	public Chromosome(int[] genes, int fitness) {
+		this.genes = genes;
+		this.fitness = fitness;
+	}
+	
+	public int[] getGenes() {
+		return genes;
+	}
+	
+	public void setGenes(int[] genes) {
+		this.genes = genes;
+	}
+	
+	public int getFitness() {
+		return fitness;
+	}
+	
+	public void setFitness(int fitness) {
+		this.fitness = fitness;
+	}
 }
