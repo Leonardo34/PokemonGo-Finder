2b840965abcb920f116131d438e705454a9a7cf4
Leonardo34 <leo_broch@hotmail.com>
2017-03-27 15:32:29 -0300

    Add IPokemonGo Interface

---
+++ src/br/com/unisinos/PokemonFinder/IPokemonGo.java
@@ -0,0 +1,13 @@
+package br.com.unisinos.PokemonFinder;
+
+import java.io.File;
+import java.io.IOException;
+
+public interface IPokemonGo {
+	public void loadMatrix(File file) throws IOException;
+	public Chromosome generateChromosome();
+	public void insertOrderedChromosome(Chromosome chromosome);
+	public int distance(int[] genes);
+	public void evolve();
+	public void print();
+}
