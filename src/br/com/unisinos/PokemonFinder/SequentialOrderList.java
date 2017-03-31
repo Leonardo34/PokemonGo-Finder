ebfe42e2feabfaaa04c56b76604f2796c1ca4234
Leonardo34 <leo_broch@hotmail.com>
2017-03-31 15:03:32 -0300

    Implemented insert method in SequentialOrderList

--- src/br/com/unisinos/PokemonFinder/SequentialOrderList.java
+++ src/br/com/unisinos/PokemonFinder/SequentialOrderList.java
@@ -2,23 +2,36 @@ package br.com.unisinos.PokemonFinder;
 
 public class SequentialOrderList<E extends Comparable<E>> implements List<E> {
 	private Object[] elements;
-	
+
 	public SequentialOrderList(int size) {
 		elements = new Object[size];
 	}
-	
+
 	@Override
 	public E get(int index) {
 		return (E) elements[index];
 	}
-	
+
 	@Override
 	public void insert(E element) {
-		
+		for (int i = 0; i < size(); i++) {
+			if (elements[i] == null || 
+					((E) elements[i]).compareTo(element) > 0) {
+				insertAt(i, element);
+				break;
+			}
+		}
 	}
-	
+
 	@Override
 	public int size() {
 		return elements.length;
+	}
+	
+	private void insertAt(int index, E element) {
+		for (int i = size() - 1; i >= index; i--) {
+			elements[i + 1] = elements[i];
+		}
+		elements[index] = element;
 	}
 }
