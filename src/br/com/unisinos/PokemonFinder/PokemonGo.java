38720ecd5fb2ed3d4f005d53e78c15f4d19cfece
Leonardo34 <leo_broch@hotmail.com>
2017-04-01 14:32:09 -0300

    Added spec and PokemonGo class

---
+++ src/br/com/unisinos/PokemonFinder/PokemonGo.java
@@ -0,0 +1,44 @@
+package br.com.unisinos.PokemonFinder;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PokemonGo implements IPokemonGo {
+
+	@Override
+	public void loadMatrix(File file) throws IOException {
+		// TODO
+
+	}
+
+	@Override
+	public Chromosome generateChromosome() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public void insertOrderedChromosome(Chromosome chromosome) {
+		// TODO
+
+	}
+
+	@Override
+	public int distance(int[] genes) {
+		// TODO
+		return 0;
+	}
+
+	@Override
+	public void evolve() {
+		// TODO 
+
+	}
+
+	@Override
+	public void print() {
+		// TODO 
+
+	}
+
+}
