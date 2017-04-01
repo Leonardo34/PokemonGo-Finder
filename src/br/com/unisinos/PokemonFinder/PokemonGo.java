aad0ebe3fd72175d3faafaa25967b74a66f973c8
Leonardo34 <leo_broch@hotmail.com>
2017-04-01 14:51:57 -0300

    Implemented loadMatrix method in PokemonGo Class

--- src/br/com/unisinos/PokemonFinder/PokemonGo.java
+++ src/br/com/unisinos/PokemonFinder/PokemonGo.java
@@ -1,13 +1,26 @@
 package br.com.unisinos.PokemonFinder;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class PokemonGo implements IPokemonGo {
+	private final int[][] matriz = new int[16][16];
 
 	@Override
 	public void loadMatrix(File file) throws IOException {
-		// TODO
+		try (BufferedReader reader = 
+				new BufferedReader(new FileReader(file))) {
+			String line;
+			int lineCount = 0;
+			while ((line = reader.readLine()) != null) {
+				String[] vert = line.split(";");
+				for (int i = 0; i < vert.length; i++) {
+					matriz[lineCount][i] = Integer.parseInt(vert[i]);
+				}
+			}
+		}
 
 	}
 
