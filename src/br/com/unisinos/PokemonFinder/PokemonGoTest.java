b783220cb87a3c030dddcf1c880f53552fb14ce3
Leonardo34 <leo_broch@hotmail.com>
2017-04-06 15:28:37 -0300

    Last changes and PokemonGoTest class added

---
+++ src/br/com/unisinos/PokemonFinder/PokemonGoTest.java
@@ -0,0 +1,19 @@
+package br.com.unisinos.PokemonFinder;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PokemonGoTest {
+	public static final String DIRECTORY = "C:/Users/Leonardo/Trab-Lab2-GrauA/Trab-Grau-A-Lab2/Matriz.txt";
+	
+	public static void main(String[] args) {
+		IPokemonGo pokemonGo = new PokemonGo();
+		try {
+			pokemonGo.loadMatrix(new File(DIRECTORY));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		pokemonGo.evolve();
+		pokemonGo.print();
+	}
+}
